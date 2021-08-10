92a22eff6f2169a46aa67514f2c102eda0ff8198
nnolte <niklas.nolte@cern.ch>
2021-08-10 16:47:35 -0400

    adjust monotonic constraints to the same API as lightgbm

--- InfinityNorm/SigmaNet.py
+++ InfinityNorm/SigmaNet.py
@@ -8,25 +8,16 @@ class SigmaNet(nn.Module):
             self,
             nn: nn.Module,  # Must already be sigma lipschitz
             sigma: float,
-            monotonic_in: T.Optional[T.Iterable] = None,
-            nfeatures: T.Optional[int] = None,
+            monotone_constraints: T.Optional[T.Iterable] = None,
             gamma: T.Optional[float] = 1
     ):
         super().__init__()
         self.nn = nn
         self.register_buffer("gamma", torch.Tensor([gamma]))
         self.register_buffer("sigma", torch.Tensor([sigma]))
-        if monotonic_in is not None:
-            assert (nfeatures is not None), ("if you want custom monotonicity, "
-                                             "define how many features "
-                                             "your data has via the nfeatures argument")
-            # only be monotonic in these indices
-            self.mask = torch.zeros(nfeatures).float()
-            self.mask[monotonic_in] = 1.0
-        else:
-            # no mask then, monotonic in all features
-            self.mask = torch.tensor([1]).float()
+        self.monotone_constraint = monotone_constraints or [1]
+        self.monotone_constraint = torch.tensor(self.monotone_constraint).float()
 
     def forward(self, x):
-        return (self.nn(x) + self.sigma * self.gamma * (x * self.mask.to(x.device)).sum(
+        return (self.nn(x) + self.sigma * self.gamma * (x * self.monotone_constraint.to(x.device)).sum(
             axis=-1, keepdim=True))
