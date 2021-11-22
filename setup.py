cc0b2bf67deb116b72952e54da4931b2ebaf4403
kitouni <okitouni@users.noreply.github.com>
2021-11-22 12:48:49 -0500

    renamed project

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='InfinityNorm',
+    name='monotonenorm',
     version='0.1.0',
-    packages=['InfinityNorm'],
+    packages=['monotonenorm'],
     url='',
     license='MIT',
     author='Ouail Kitouni, Niklas Nolte',
