e7d260c52a98054dc62a6485818c79bbc0dd90d5
kitouni <28654657+okitouni@users.noreply.github.com>
2022-09-25 11:12:34 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='monotonenorm',
-    version='0.1.0',
+    version='1.0.0',
     packages=['monotonenorm'],
     url='',
     license='MIT',
     author='Ouail Kitouni, Niklas Nolte',
     author_email='kitouni@mit.edu, nnolte@mit.edu',
-    description='Pytorch Implementation of Infinity norm for robustness and monotonicity'
+    description='Pytorch Implementation of constrained weight operator norms for robustness and monotonicity'
 )
